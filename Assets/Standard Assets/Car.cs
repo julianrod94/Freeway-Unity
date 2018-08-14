57033c9084f5ccca20760ebbc48a272bf72c5372
AngieArlanti <angeles.arlanti@mercadolibre.com>
2018-08-14 20:33:21 -0300

    Cars working in both ways

--- Assets/Standard Assets/Car.cs
+++ Assets/Standard Assets/Car.cs
@@ -1,27 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Car : MonoBehaviour {
 	
-	public int speed;
+	public float initialSpeed;
+	private float currentSpeed;
+	private int limit;
+	
+	public Direction direction;
+
+	public enum Direction
+	{
+		LEFT,RIGHT
+	}
 
 	private Vector3 initialPosition;
 
 	// Use this for initialization
 	void Start ()
 	{
+		if (direction == Direction.LEFT)
+		{
+			initialSpeed = -initialSpeed;
+		}
+		currentSpeed = initialSpeed;
+
+		
 		initialPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var x = Time.deltaTime * speed;
-		if (transform.position.x + x < -11)
+		var rand = new Random();
+
+		if (Random.value < 0.01)
 		{
-			return;
-		} 
-		if (transform.position.x + x > 11)
+			currentSpeed = initialSpeed * (0.8f + Random.value * 0.4f);
+		}
+
+		var sceenWidth = Screen.width;
+		
+		
+		var x = Time.deltaTime * currentSpeed;
+		
+		
+		var vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;    
+		var horzExtent = vertExtent * Screen.width / Screen.height;
+
+		if (Math.Abs(transform.position.x) > Math.Abs(horzExtent*1.2))
 		{
 			transform.position = initialPosition;
 		}
