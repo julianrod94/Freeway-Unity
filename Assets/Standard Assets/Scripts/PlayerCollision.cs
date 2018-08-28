531790ad368fe15a5df480f8dcb2bd24d490cce9
AngieArlanti <angeles.arlanti@mercadolibre.com>
2018-08-28 20:28:01 -0300

    PlayerCollision Sound

--- Assets/Standard Assets/Scripts/PlayerCollision.cs
+++ Assets/Standard Assets/Scripts/PlayerCollision.cs
@@ -5,6 +5,7 @@ namespace Standard_Assets.Scripts {
         private void OnCollisionEnter2D(Collision2D other) {
             if (other.gameObject.CompareTag("Car")) {
                 gameObject.GetComponent<PlayerController>().CrashWithCar();
+                AudioManager.Instance.playPlayerDeath();
             }
         }
     }
