86faaa9630ef5cda1bc17ede054dc3ed68ffed0b
jcaracciolo <jcaracciolo@itba.edu.ar>
2018-08-28 20:49:56 -0300

    Fix speed add end screen

--- Assets/Standard Assets/Scripts/LevelScoreManager.cs
+++ Assets/Standard Assets/Scripts/LevelScoreManager.cs
@@ -7,9 +7,27 @@ namespace Standard_Assets.Scripts {
         public Text TextScoreP1;
         public Text TextScoreP2;
 
+        public Image Player1Wins;
+        public Image Player2Wins;
+        public Image Draw;
+
         public void Update() {
-            TextScoreP1.text = ScoreManager.Instance.P1Score.ToString();
-            TextScoreP2.text = ScoreManager.Instance.P2Score.ToString();
+            var p1Score = ScoreManager.Instance.P1Score;
+            var p2Score = ScoreManager.Instance.P2Score;
+            
+            switch (GameManager.Instance.State) {
+                case GameState.Playing:
+                    TextScoreP1.text = p1Score.ToString();
+                    TextScoreP2.text = p2Score.ToString();
+                    break;
+                case GameState.Score:
+                    Player1Wins.enabled = p1Score > p2Score;
+                    Player2Wins.enabled = p1Score < p2Score;
+                    Draw.enabled = p1Score == p2Score;
+                    break;
+            }
+            
+            
         }
     }
 }
